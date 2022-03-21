b719ba71ee409105b497ec02d7eaeac68c82b9c6
Junsang-mun <mun.js@aol.com>
2022-03-21 22:13:40 +0900

    update main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,91 +1,11 @@
 <template>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    <h1>asldkfjlaskdfjlkasjfd</h1>
-    
+    <header class="masthead">
+        <div class="container">
+            <div class="masthead-subheading">Welcome To Our Studio!</div>
+            <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+            <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -97,3 +17,50 @@
         }
     }
 </script>
+
+<style>
+header.masthead {
+  padding-top: 10.5rem;
+  padding-bottom: 6rem;
+  text-align: center;
+  color: wheat;
+  background-image: url("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg");
+  background-repeat: no-repeat;
+  background-attachment: scroll;
+  background-position: center center;
+  background-size: cover;
+}
+header.masthead .masthead-subheading {
+  font-size: 1.5rem;
+  font-style: italic;
+  line-height: 1.5rem;
+  margin-bottom: 25px;
+  font-family: "Roboto Slab", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+header.masthead .masthead-heading {
+  font-size: 3.25rem;
+  font-weight: 700;
+  line-height: 3.25rem;
+  margin-bottom: 2rem;
+  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+
+@media (min-width: 768px) {
+  header.masthead {
+    padding-top: 17rem;
+    padding-bottom: 12.5rem;
+  }
+  header.masthead .masthead-subheading {
+    font-size: 2.25rem;
+    font-style: italic;
+    line-height: 2.25rem;
+    margin-bottom: 2rem;
+  }
+  header.masthead .masthead-heading {
+    font-size: 4.5rem;
+    font-weight: 700;
+    line-height: 4.5rem;
+    margin-bottom: 4rem;
+  }
+}
+</style>
