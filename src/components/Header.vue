31de33a35f563f3740c4e85c5cee7d13ec717ad5
Junsang-mun <mun.js@aol.com>
2022-03-22 17:31:41 +0900

    href changes when clicking logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header class="p-3 bg-dark text-white sticky-top" style="border-bottom-color: aliceblue; border-bottom:solid;">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="./index.html" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
                     <img src="https://images.velog.io/images/mun_js/post/2dadbc64-a8aa-4939-ab8b-5998c5a6cc48/gschool.svg" style="height: 1.5rem; padding-right: 3rem;">
                     <!-- https://images.velog.io/images/mun_js/post/a17afcd6-dbed-4e4f-9ec1-e4972973fd0f/gschool-black.svg -->
                 </a>
