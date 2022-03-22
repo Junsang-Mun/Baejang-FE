29cbb65e785d331cf27337a1adf47ebf72b98c1a
Junsang-mun <mun.js@aol.com>
2022-03-22 17:35:06 +0900

    add footer

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
   <div id="content" class="content">
     <router-view></router-view>
   </div>
+  <Footer />
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Footer,
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <footer class="row row-cols-5 py-5 my-5 border-top">
+          <div class="col">
+            <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+              <img src="https://images.velog.io/images/mun_js/post/a17afcd6-dbed-4e4f-9ec1-e4972973fd0f/gschool-black.svg" style="height: 2rem;">
+            </a>
+            <p class="text-muted">© 2022. Junsang Mun. All Rights Reserved.</p>
+            <p class="text-muted">Trademarks that contain'거꾸로캠퍼스' or 'G-School' or something similar is a trademark of 거꾸로캠퍼스.</p>
+          </div>
+      
+          <div class="col">
+      
+          </div>
+      
+          <div class="col">
+            <h5><a href="./blog/index.html" class="text-muted hover-white">Blog</a></h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">팀 세모나다</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">팀 세모나다</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">팀 세모나다</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">팀 세모나다</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">팀 세모나다</a></li>
+            </ul>
+          </div>
+      
+          <div class="col">
+            <h5 class="text-muted">Links</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">어쩌고</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">저쩌고</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">어쩌고</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">저쩌고</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted hover-white">어쩌고</a></li>
+            </ul>
+          </div>
+      
+          <div class="col">
+            <h5 class="text-muted">Links</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="https://거꾸로캠퍼스.kr" class="nav-link p-0 text-muted hover-white">거꾸로캠퍼스 홈페이지</a></li>
+              <li class="nav-item mb-2"><a href="https://gschool.kr" class="nav-link p-0 text-muted hover-white">사단법인 교육실험실21 홈페이지</a></li>
+              <li class="nav-item mb-2"><a href="https://youtube.com" class="nav-link p-0 text-muted hover-white">거꾸로캠퍼스 YouTube</a></li>
+              <li class="nav-item mb-2"><a href="https://google.com" class="nav-link p-0 text-muted hover-white">배움장터 예약 폼</a></li>
+            </ul>
+          </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    module.exports ={
+        data: function(){
+            return{
+            
+            }
+        }
+    }
+</script>
+
+<style>
+.f-cover{
+    height: 90vh;
+    background-image: url(https://cdn.pixabay.com/photo/2020/11/12/16/58/worker-5736096_1280.jpg); 
+    background-size: cover;
+}
+</style>
