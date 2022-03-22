26aa9c7f6c4000a0d739c5f862d0f3af82181e24
Junsang-mun <mun.js@aol.com>
2022-03-22 17:32:01 +0900

    pages change

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="container col-xxl-8 px-4 py-5" style="background-image: url(https://cdn.pixabay.com/photo/2020/11/12/16/58/worker-5736096_1280.jpg); background-size: cover;">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-lg-6">
-        <h1 class="display-5 h-b lh-1 mb-3 text-white">거꾸로캠퍼스에서 배움장터의 의미는</h1>
-        <p class="lead n-m text-white" style="background-color: rgba(0,0,0,0.3);">단순한 발표회 그 이상입니다. 학생들이 1개 모듈동안 배운 세상의 지식을 가감없이 뽐내는 자리이며, 거꾸로캠퍼스 학생들이 얼마나 성장했는지를 보여주는 척도입니다. 이번 2022년 2모듈 거꾸로캠퍼스 배움장터에 여러분을 초대할 수 있어 영광입니다. </p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <a type="button" class="btn btn-warning btn-lg px-4 me-md-2" href="/rsvp">더 알아보기</a>
-          <a type="button" class="btn btn-outline-secondary btn-lg px-4" href="/rsvp">배움장터 예약하기</a>
+<div class="">
+  <div class="f-cover">
+    <div class="container col-xxl-8 px-4 py-5">
+      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-lg-6">
+          <h1 class="display-5 h-b lh-1 mb-3 text-white">거꾸로캠퍼스에서 배움장터의 의미는</h1>
+          <p class="lead n-m text-white" style="background-color: rgba(0,0,0,0.3);">
+            단순한 발표회 그 이상입니다. 학생들이 1개 모듈동안 배운 세상의 지식을 가감없이 뽐내는 자리이며, 거꾸로캠퍼스 학생들이 얼마나 성장했는지를 보여주는 척도입니다.<br>이번 2022년 2모듈 거꾸로캠퍼스 배움장터에 여러분을 초대할 수 있어 영광입니다.
+            </p>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <a type="button" class="btn btn-warning btn-lg px-4 me-md-2" href="/rsvp">더 알아보기</a>
+            <a type="button" class="btn btn-outline-secondary btn-lg px-4" href="/rsvp">배움장터 예약하기</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -24,4 +30,9 @@
 </script>
 
 <style>
+.f-cover{
+    height: 90vh;
+    background-image: url(https://cdn.pixabay.com/photo/2020/11/12/16/58/worker-5736096_1280.jpg); 
+    background-size: cover;
+}
 </style>
