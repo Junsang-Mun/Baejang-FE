d7f8408774a7ad9e132b3fd878f2d9e54c37a9a5
Junsang-mun <mun.js@aol.com>
2022-03-23 21:19:14 +0900

    fix padding problem

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,10 @@
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <img src="https://images.velog.io/images/mun_js/post/2dadbc64-a8aa-4939-ab8b-5998c5a6cc48/gschool.svg" style="height: 1.5rem; padding-right: 3rem;">
+                    <img src="https://images.velog.io/images/mun_js/post/2dadbc64-a8aa-4939-ab8b-5998c5a6cc48/gschool.svg" style="height: 1.5rem; ">
                     <!-- https://images.velog.io/images/mun_js/post/a17afcd6-dbed-4e4f-9ec1-e4972973fd0f/gschool-black.svg -->
                 </a>
+                <div style="width: 3rem;" class="d-none d-xxl-block d-xl-block d-lg-block"> </div>
             
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="/" class="nav-link px-2 text-white hover">대문</a></li>
@@ -34,5 +35,8 @@
     transition: 300ms;
     background-color: #adadad;
     cursor: pointer;
+}
+.justify-content-center{
+    justify-content: center;
 }
 </style>
